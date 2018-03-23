3a81d14afb1d3e0423037e31c694abe190cad26b
Zhancheng Song <zhancheng.song@ibm.com>
2018-03-23 13:14:11 -0400

    Added the Sunshine project and cleandup

--- Exercises/ud851-Exercises/Lesson03-Green-Recycler-View/T03.02-Exercise-ViewHolder/app/src/main/java/com/example/android/recyclerview/GreenAdapter.java
+++ Exercises/ud851-Exercises/Lesson03-Green-Recycler-View/T03.02-Exercise-ViewHolder/app/src/main/java/com/example/android/recyclerview/GreenAdapter.java
@@ -118,14 +118,23 @@ public class GreenAdapter extends RecyclerView.Adapter<GreenAdapter.NumberViewHo
     }
 
     // TODO (12) Create a class called NumberViewHolder that extends RecyclerView.ViewHolder
-
+    public class NumberViewHolder extends RecyclerView.ViewHolder {
     // TODO (13) Within NumberViewHolder, create a TextView variable called listItemNumberView
-
+        private TextView listItemNumberView;
     // TODO (14) Create a constructor for NumberViewHolder that accepts a View called itemView as a parameter
     // TODO (15) Within the constructor, call super(itemView) and then find listItemNumberView by ID
+        public NumberViewHolder(View itemView) {
+            super(itemView);
+            listItemNumberView = (TextView) itemView.findViewById(R.id.tv_item_number);
+
+        }
 
     // TODO (16) Within the NumberViewHolder class, create a void method called bind that accepts an int parameter called listIndex
-    // TODO (17) Within bind, set the text of listItemNumberView to the listIndex
+        public void bind(int listIndex){
+            listItemNumberView.setText(String.valueOf(listIndex));
+
+        }
+        // TODO (17) Within bind, set the text of listItemNumberView to the listIndex
     // TODO (18) Be careful to get the String representation of listIndex, as using setText with an int does something different
 
     }

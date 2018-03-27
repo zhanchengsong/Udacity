fc1fcdc0c035a8da766ee7d088a529dc672a299b
Zhancheng Song <zhanchengsong@Zhanchengs-MacBook-Pro.local>
2018-03-26 23:35:17 -0400

    Created the base project for Popular Movies

--- Projects/Sunshine/S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ Projects/Sunshine/S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -2,16 +2,19 @@ package com.example.android.sunshine;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class DetailActivity extends AppCompatActivity {
 
     private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
-
+    private TextView mWeatherDisplay;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-
+        mWeatherDisplay = (TextView) findViewById(R.id.tv_weather_data);
         // TODO (2) Display the weather forecast that was passed from MainActivity
     }
 }
